772e160a28123e919920d70c8b196caaabc5a15c
Anand kumar satapthy <anandkumarsatapathy@gmail.com>
2022-05-28 23:43:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// selected the row and column
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
+let topLeftCell = document.querySelector(".top-left-cell");
+let allCells = document.querySelectorAll(".cell");
+let addressInput = document.querySelector("#address");
+
+
+
+cellsContentDiv.addEventListener("scroll",function(e) {
+    let scrollFromTop = e.target.scrollTop;
+    let scrollFromLeft = e.target.scrollLeft;
+    topRow.style.top = scrollFromTop+"px";
+    leftCol.style.left = scrollFromLeft+"px";
+    topLeftCell.style.top = scrollFromTop+"px";
+    topLeftCell.style.left = scrollFromLeft+"px";
+})
+for(let i=0;i<allCells.length;i++)
+{
+    allCells[i].addEventListener("click",function(e){
+        let {rowId,colId} = getRowIdColIdFromElement(e.target);
+
+        let address = String.fromCharCode(65+parseInt(colId))+(parseInt(rowId)+1)+"";
+        addressInput.value = address;
+    })
+    allCells[i].addEventListener("blur", function(e) {
+        let cellValue = e.target.textContent;
+        
+        let {rowId,colId} = getRowIdColIdFromElement(e.target);
+        let cellObject = db[rowId][colId];
+        if(cellObject.value == cellValue){
+            return;
+        }
+        cellObject.value = cellValue;
+        console.log("After UPdate",cellObject);
+    })
+}
+
+
+
+function  getRowIdColIdFromElement(element) {    
+    let rowId = element.getAttribute("rowid");
+    let colId = element.getAttribute("colid");
+    return {
+        rowId,colId
+    }
+}
